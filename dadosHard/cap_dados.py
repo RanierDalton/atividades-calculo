bae0839db589d4d0d7d90ea415fb9840066e9754
RanierDalton <ranier.couto@sptech.school>
2025-02-21 19:55:50 -0300

    Adicionar apenas as informações que vou coletar

--- dadosHard/cap_dados.py
+++ dadosHard/cap_dados.py
@@ -20,47 +20,24 @@ def armazenar_dados(minutos_parados, porcentagem_cpu):
 
 def captar_dados():
     while True:
-        # Cada atributo representa os segundos que a CPU gastou no modo determinado.
-        cpu_user_time = math.trunc(psutil.cpu_times(percpu = False).user / 60)
-        # print(f'Sua CPU ficou {cpu_user_time:.0f} minutos parados')
+        # Conteúdos cpu 
+        porcentagem_uso_cpu = psutil.cpu_percent(interval = None, percpu = False)
 
-        cpu_usage_percentage = math.trunc(psutil.cpu_percent(interval=None))
-        # print(f'Você está utilizando {cpu_usage_percentage:.2f}% da sua CPU')
+        tempo_ativo_cpu = math.trunc(psutil.cpu_times(percpu = False).user / 60)
 
-        armazenar_dados(cpu_user_time, cpu_usage_percentage)
+        temperatura_cpu = psutil.sensors_temperatures(fahrenheit=False)['coretemp'][0].current
 
-        '''
-        percentage_memory = psutil.virtual_memory().percent
-        free_memory_ram = math.trunc(psutil.virtual_memory().free/(1024**3)) + math.trunc(psutil.virtual_memory().inactive/(1024**3))
-        print(f'Você está utilizando {percentage_memory:.2f}% da sua RAM')
-        print(f'Você tem {free_memory_ram}GiB de memória sobrando da sua RAM')
 
-        used_disk_percentage = psutil.disk_usage('/').percent
-        free_memory_disk = math.trunc(psutil.disk_usage('/').free/(1024**3))
-        print(f'Você está utilizando {used_disk_percentage:.2f}% do seu Disco Rígido')
-        print(f'Você tem {free_memory_disk}GiB de memória sobrando do seu Disco Rígido')
-        '''
+        # Conteúdos RAM
+        porcentagem_uso_ram = psutil.virtual_memory().percent
+
+        memoria_ram_usada = math.trunc(psutil.virtual_memory().used/(1024**3))
+
+        memoria_ram_total = math.trunc(psutil.virtual_memory().total/(1024**3))
+
+        armazenar_dados(cpu_user_time, cpu_usage_percentage) # todo
+
         time.sleep(30)
 
-'''
-# utilização atual da CPU
-    cpu_usage = psutil.cpu_percent(interval = None, percpu = False)
-
-    # número de CPUs lógicas no sistema
-    cpu_count = psutil.cpu_count(logical=False)
-
-    # frequência da CPU como um nome duplo
-    cpu_hz = psutil.cpu_freq()
-
-    # Disoc e Memória
-    psutil.disk_partitions()
-
-    # Retorna estatísticas de uso de disco sobre a partição que contém o caminho dado
-    # print(psutil.disk_usage('/').total/1024**2) # Megas
-
-    # print(psutil. disk_partitions(all = True))
-    # print(psutil.virtual_memory().total/(1024**3)) # Gigas
-    # print(psutil.swap_memory().total / 1024**3)
-'''   
 if __name__ == '__main__':
     captar_dados()
